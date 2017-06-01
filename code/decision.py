f9c963e9cadfd616a4117e83432269f33cb9d767
Shubo Chao <apiggy0204@gmail.com>
2017-06-02 00:38:17 +0800

    Move backward if stucked

--- code/decision.py
+++ code/decision.py
@@ -1,4 +1,5 @@
 import numpy as np
+import time
 
 
 # This is where you can build a decision tree for determining throttle, brake and steer
@@ -8,6 +9,7 @@ def decision_step(Rover):
     # Implement conditionals to decide what to do given perception data
     # Here you're all set up with some basic functionality but you'll need to
     # improve on this decision tree to do a good job of navigating autonomously!
+    print(Rover.mode)
 
     # Example:
     # Check if we have vision data to make decisions with
@@ -16,6 +18,9 @@ def decision_step(Rover):
         if Rover.mode == 'forward':
             # Check the extent of navigable terrain
             if len(Rover.nav_angles) >= Rover.stop_forward:
+                # Get stuck for over 2 second, try go backward
+                if Rover.vel < 0.2 and time.time() - Rover.forward_ts > 5:
+                    Rover.mode = 'backward'
                 # If mode is forward, navigable terrain looks good
                 # and velocity is below max, then throttle
                 if Rover.vel < Rover.max_vel:
@@ -35,15 +40,25 @@ def decision_step(Rover):
                     Rover.steer = 0
                     Rover.mode = 'stop'
 
+        elif Rover.mode == 'backward':
+            if Rover.vel > -0.5:
+                Rover.steer = 0
+                Rover.throttle = -Rover.throttle_set
+            elif Rover.vel <= -0.5:
+                Rover.throttle = 0
+                Rover.brake = Rover.brake_set
+                Rover.steer = 0
+                Rover.mode = 'stop'
+
         # If we're already in "stop" mode then make different decisions
         elif Rover.mode == 'stop':
             # If we're in stop mode but still moving keep braking
-            if Rover.vel > 0.2:
+            if Rover.vel > 0.2 or Rover.vel < -0.2:
                 Rover.throttle = 0
                 Rover.brake = Rover.brake_set
                 Rover.steer = 0
             # If we're not moving (vel < 0.2) then do something else
-            elif Rover.vel <= 0.2:
+            elif Rover.vel <= 0.2 and Rover.vel >= -0.2:
                 # Now we're stopped and we have vision data to see if there's a path forward
                 if len(Rover.nav_angles) < Rover.go_forward:
                     Rover.throttle = 0
@@ -60,6 +75,7 @@ def decision_step(Rover):
                     # Set steer to mean angle
                     Rover.steer = np.clip(np.mean(Rover.nav_angles * 180/np.pi), -15, 15)
                     Rover.mode = 'forward'
+                    Rover.forward_ts = time.time()
     # Just to make the rover do something
     # even if no modifications have been made to the code
     else:
